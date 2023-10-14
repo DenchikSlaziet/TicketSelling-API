2c0ccd4e560d712488ff6f0d67223d7131b8123c
DenchikSlaziet <kochetkovdenic1@gmail.com>
2023-10-14 19:06:34 +0300

    Настроил расширения

---
+++ TicketSelling.Context/RegistrationContexts.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketSelling.Context.Contracts;
+
+namespace TicketSelling.Context
+{
+    public static class RegistrationContexts
+    {
+        public static void RegistrationContext(this IServiceCollection service)
+        {
+            service.AddSingleton<ITicketSellingContext, TicketSellingContext>();
+        }
+    }
+}
---
+++ TicketSelling.Services/RegistrationService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TicketSelling.General;
+using TicketSelling.Services.Anchors;
+
+namespace TicketSelling.Services
+{
+    public static class RegistrationService
+    {
+        public static void RegistrationServices(this IServiceCollection service)
+        {
+            service.RegistrationOnInterface<IServiceAnchor>(ServiceLifetime.Scoped);
+        }       
+    }
+}
