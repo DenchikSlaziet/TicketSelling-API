d2ece0266cea5bfba31b786936c553e29387ba25
DenchikSlaziet <kochetkovdenic1@gmail.com>
2023-10-27 20:00:25 +0300

    Поменял жизненный цикл контекста

--- TicketSelling.API/Program.cs
+++ TicketSelling.API/Program.cs
@@ -11,7 +11,7 @@ builder.Services.RegistrationControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.RegistrationSwagger();
 builder.Services.AddDbContextFactory<TicketSellingContext>(options
-    => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")), ServiceLifetime.Transient);
 builder.Services.RegistrationSRC();
 
 var app = builder.Build();
